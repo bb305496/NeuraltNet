00040d52a5e3cdf4007c151c535557ac7e97f5fd
bb305496 <bartekbielak12@onet.pl>
2024-06-02 17:51:12 +0200

    MSE loss

--- main/main/main.cpp
+++ main/main/main.cpp
@@ -1,6 +1,19 @@
 ﻿#include <iostream>
+#include <cmath>
 #include "Neuron.h"
 #include "NeuralNet.h"
+
+float mse_loss(std::vector<int> y_true, std::vector<int> y_pred)
+{
+	float sum = 0;
+
+	for (int i = 0; i < y_true.size(); i++)
+	{
+		sum += std::pow((y_true[i] - y_pred[i]), 2);
+	}
+
+	return (sum / y_true.size());
+}
 
 int main()
 {
@@ -13,6 +26,11 @@ int main()
 	std::cout << n.feedForward(x) << "\n";
 
 	NeuralNet network;
-	std::cout << network.feedForward(x);
+	std::cout << network.feedForward(x) << "\n";
+
+	std::vector<int> y_true = { 1,0,0,1 };
+	std::vector<int> y_pred = { 0,0,0,0 };
+
+	std::cout << mse_loss(y_true, y_pred);
 }
 
